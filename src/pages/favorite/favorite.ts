af21c0ed95148fd2a805c91fe1e4c18db35d376c
HeshamEG <heshamsalama1212901022@gmail.com>
2017-12-02 18:51:34 +0200

    fav

--- src/pages/favorite/favorite.ts
+++ src/pages/favorite/favorite.ts
@@ -12,13 +12,14 @@ export class FavoritePage {
 
   constructor(public common:CommonservicesProvider,public sups:SupsProvider,public navCtrl: NavController, public navParams: NavParams) {
   }
+    member_id:any
 favs:any
   ionViewWillEnter() {
       this.favs=[]
 let self=this
     this.common.getStoredValue('user').then(user=>{
         console.log(user.member_id)
-
+self.member_id=user.member_id
         self.sups.getUserFav(user.member_id).subscribe(res=>{
           console.log(res)
             self.favs=res
@@ -27,15 +28,19 @@ let self=this
     })
     console.log('ionViewDidLoad FavoritePage');
   }
-    deleteToUserFav(itemToBeDeleted){
+    deleteToUserFav(itemToBeDeleted,index){
         let fav={
-            "member_id":this.member_id,"supply_id":itemToBeDeleted
+            "member_id":this.member_id,
+            "supply_favorite_id":itemToBeDeleted
         }
 
         this.sups.deleteToUserFav(fav).subscribe(res=>{
+            console.log(fav)
             console.log(res)
-  // this.favs.splice()
-            this.common.presentToast('تمت الحذف من قائمة المفضلة')
+            // console.log(res.error)
+            if(res['error']==null){
+  this.favs.splice(index,1)
+            this.common.presentToast('تمت الحذف من قائمة المفضلة')}
         })
 
   }
