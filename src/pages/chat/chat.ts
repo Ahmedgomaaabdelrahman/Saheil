2adb255bb34782965226c51c73257010597338c2
HeshamEG <heshamsalama1212901022@gmail.com>
2017-12-22 18:21:08 +0200

    c

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {ChatProvider} from "../../providers/chat/chat";
+import {CommonservicesProvider} from "../../providers/commonservices/commonservices";
 
 
 @Component({
@@ -9,11 +10,16 @@ import {ChatProvider} from "../../providers/chat/chat";
 })
 export class ChatPage {
 msgs
-  constructor(private chat:ChatProvider,public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private common:CommonservicesProvider,private chat:ChatProvider,public navCtrl: NavController, public navParams: NavParams) {
 
   }
-
+member_id
   ionViewWillEnter() {
+  this.common.getStoredValue('user').then(res=>{
+    this.member_id=res.member_id
+      console.log(this.member_id)
+
+  })
 this.msgs=[]
     console.log('ionViewDidLoad ChatPage');
 this.chat.getPublicChat().subscribe(res=>{
